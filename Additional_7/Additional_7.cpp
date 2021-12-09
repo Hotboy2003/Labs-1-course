bc1e12c5457dec6fe105f844bb6e81babc0a9dc5
Alexandr <alex.obyn@yandex.ru>
2021-12-09 13:07:07 +0200

    Добавлен 7 адишнл

---
+++ Additional_7/Additional_7.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector<int> v;
+
+bool Cifra8(int n)
+{
+    while (n != 0)
+    {
+        if (n % 10 == 8)
+        {
+            return true;
+        }
+        n = n / 10;
+    }
+    return false;
+}
+
+long int ProizCifr(int n) {
+    long int ans = 1;
+    while (n != 0) {
+        ans *= n % 10;
+        n /= 10;
+    }
+    return ans;
+}
+
+int main() {
+    int n, a;
+    
+    std::cin >> n;
+    for (int i = 0; i < n * 2; i += 2) {
+        std::cin >> a;
+        v.push_back(a);
+    }
+    for (int i = 0; i < v.size(); i++) {
+        if (ProizCifr(v[i]) == 144) {
+            v.erase(v.begin() + i);
+            i++;
+        }
+    }
+    for (int i = 0; i < v.size(); i++) {
+        if (Cifra8(v[i]) == true) {
+            v.insert(v.begin() + i, v[i]);
+            i++;
+        }
+    }
+    for (auto& v : v)
+        std::cout << v << std::endl;
+}
