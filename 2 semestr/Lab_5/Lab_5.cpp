0ea120de8a78c6484b2ac650004bac5ff59f0dfc
Alexandr <alex.obyn@yandex.ru>
2022-03-12 11:37:34 +0200

    Доделал 5 лабу

--- 2 semestr/Lab_5/Lab_5.cpp
+++ 2 semestr/Lab_5/Lab_5.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <cstring> 
+#include <string> // подключаем строки
 
 struct Elem
 {
@@ -62,13 +65,141 @@ void PASS(Elem* v)
     PASS(v->right);
 }
 
+Elem* DEEP(int data, Elem* v, int& k) // v - ,    
+{
+    k += 1;
+    if (v == nullptr)
+        return v;
+    if (v->data == data)
+        return v;
+    if (data < v->data)
+    {
+        return DEEP(data, v->left, k);
+    }
+    else
+    {
+        return DEEP(data, v->right, k);
+    }
+}
+
+//Поиск элемента
+Elem* SEARCH(int data, Elem* v) // v - ,    
+{
+    if (v == nullptr)
+        return v;
+    if (v->data == data)
+        return v;
+    if (data < v->data)
+        return SEARCH(data, v->left);
+    else
+        return SEARCH(data, v->right);
+}
+
+//Удаление
+void DELETE(int data, Elem*& root)
+{
+    //   .  
+    Elem* u = SEARCH(data, root);
+    if (u == nullptr)
+        return;
+
+    //   ( )
+    if (u->left == nullptr && u->right == nullptr && u == root)
+    {
+        delete root;
+        root = nullptr;
+        return;
+    }
+
+    //   
+    if (u->left == nullptr && u->right != nullptr && u == root) // TODO  u==root
+    {
+        Elem* t = u->right;
+        while (t->left != nullptr)
+            t = t->left;
+        u->data = t->data;
+        u = t;
+    }
+
+    //   
+    if (u->left != nullptr && u->right == nullptr && u == root) // TODO  u==root
+    {
+        Elem* t = u->left;
+        while (t->right != nullptr)
+            t = t->right;
+        u->data = t->data;
+        u = t;
+    }
+
+    //    
+    if (u->left != nullptr && u->right != nullptr)
+    {
+        Elem* t = u->right;
+        while (t->left != nullptr)
+            t = t->left;
+        u->data = t->data;
+        u = t;
+    }
+    Elem* t;
+    if (u->left == nullptr)
+        t = u->right;
+    else
+        t = u->left;
+    if (u->parent->left == u)
+        u->parent->left = t;
+    else
+        u->parent->right = t;
+    if (t != nullptr)
+        t->parent = u->parent;
+    delete u;
+}
 
 
+
+void CLEAR(Elem*& v)
+{
+    if (v == nullptr)
+        return;
+    CLEAR(v->left);
+
+    CLEAR(v->right);
+
+    delete v;
+    v = nullptr;
+}
 
 
 int main()
 {
     Elem* root = nullptr; //Создание корня
-    
+
+    std::ifstream file("1.txt");
+    std::ofstream fin("2.txt");
+    std::string line;
+    while (file>>line)
+    {
+        if (line[0] == 'E')
+            break;
+        if (line[0] == '+')
+            ADD(std::stoi(line), root);
+        if (line[0] == '-')
+            DELETE(std::stoi(line.substr(1, line.length() - 1)), root);
+        if (line[0] == '?')
+        {
+            int k = 0;
+            if ((SEARCH(std::stoi(line.substr(1, line.length() - 1)), root) == nullptr))
+                fin << "n";
+            else
+            {
+                DEEP(std::stoi(line.substr(1, line.length() - 1)), root, k);
+                fin << k;
+            }
+        }
+    }
+
+    PASS(root);
+
+    CLEAR(root);
+
     return 0;
 }
