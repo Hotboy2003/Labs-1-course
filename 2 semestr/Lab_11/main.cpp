89b91fac00d00dbf3d0d8519145e8f5d448b34e1
Alexandr <alex.obyn@yandex.ru>
2022-04-26 12:39:35 +0200

    Возможно сделал 11 лабу

---
+++ 2 semestr/Lab_11/main.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+
+class JAVA
+{
+public:
+    JAVA()
+    {
+
+    }
+
+    virtual void PRINTHELLOWORLD()
+    {
+        std::cout << "java hello world" << std::endl;
+    }
+
+    virtual ~JAVA()
+    {
+
+    }
+
+protected:
+};
+
+class C_PLUS_PLUS : public JAVA
+{
+public:
+    C_PLUS_PLUS() : JAVA()
+    {
+
+    }
+
+    void PRINTHELLOWORLD() override
+    {
+        std::cout << "c_plus_plus hello world" << std::endl;
+    }
+
+    virtual ~C_PLUS_PLUS() 
+    {
+
+    }
+protected:
+};
+
+class PHP : public JAVA
+{
+public:
+    PHP() : JAVA()
+    {
+
+    }
+
+    void PRINTHELLOWORLD() override
+    {
+        std::cout << "PHP hello world" << std::endl;
+    }
+
+    virtual ~PHP()
+    {
+
+    }
+protected:
+};
+
+
+
+int main()
+{
+    JAVA java;
+    java.PRINTHELLOWORLD();
+
+    C_PLUS_PLUS cplusplus;
+    cplusplus.PRINTHELLOWORLD();
+
+    PHP php;
+    php.PRINTHELLOWORLD();
+
+    return 0;
+}
